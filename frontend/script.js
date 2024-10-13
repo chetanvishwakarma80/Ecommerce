994f35c256d2d1329db3e6b676c37c5a5a5657c8
chetanvishwakarma80 <chetanvishwakarma979@gmail.com>
2024-10-13 13:16:17 +0530

    This is my code

---
+++ frontend/script.js
@@ -0,0 +1,29 @@
+const carouselItems = document.querySelectorAll(".carousel_item"); 
+let i = 1;
+
+setInterval(() => {
+// Accessing All the carousel Items
+ Array.from(carouselItems).forEach((item,index) => {
+
+   if(i < carouselItems.length){
+    item.style.transform = `translateX(-${i*100}%)`
+   }
+  })
+
+
+  if(i < carouselItems.length){
+    i++;
+  }
+  else{
+    i=0;
+  }
+},2000)
+
+
+const burger = document.getElementById('burger');
+        const navLinks = document.getElementById('navLinks');
+
+        burger.addEventListener('click', () => {
+            navLinks.classList.toggle('nav-active');
+        });
+// carousel
